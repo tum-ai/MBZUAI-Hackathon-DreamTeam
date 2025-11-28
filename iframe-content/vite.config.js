e68b762e7ce8dac19025bc11a505407d6042ff3b
Kesava Prasad Arul <kesavaprasadarul@outlook.com>
2025-11-28 10:56:21 +0100

    Fix port numbers

--- iframe-content/vite.config.js
+++ iframe-content/vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //change port for production
+  preview: {
+    port: 5174,
+  },
+  // for dev
+  server: {
+    port: 5174,
+  },
 })
